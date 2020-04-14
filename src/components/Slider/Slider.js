c00afa30598569be820c84ea1a4fb9cadf6e4bbd
salikovskiy <ssalikovskiy@gmail.com>
2020-04-14 12:48:22 +0300

    slider bassically done

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,5 +1,20 @@
 import React from "react";
+import Carousel from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
+import css from "./Slider.module.css";
 
-const Slider = () => <h1>slider</h1>;
+const Slider = () => (
+  <Carousel autoPlay={4000} animationSpeed={1000} infinite>
+    <div className={`${css.imageOne} ${css.slider}`}>
+      <p className={css.sliderText}>Стиль</p>
+    </div>
+    <div className={`${css.imageTwo} ${css.slider}`}>
+      <p className={css.sliderText}>Вишуканість</p>
+    </div>
+    <div className={`${css.imageThree} ${css.slider}`}>
+      <p className={css.sliderText}>Зручність</p>
+    </div>
+  </Carousel>
+);
 
 export default Slider;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,8 +7,8 @@ class Homepage extends Component {
   render() {
     return (
       <div className="container">
-        <Slider />
         <Baner />
+        <Slider />
       </div>
     );
   }
